264c2069bbbfe05daff53bee14c112c330ede162
nikjavor <nik.javor+git@proton.me>
2026-04-24 12:31:20 +0200

    Setup basic server

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from "@/app";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`
+=================================
+🚀 Server running
+🌐 http://localhost:${PORT}
+=================================
+  `);
+});
